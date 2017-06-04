4a2d2317f4db0189145d821412b49ebf3f38e88c
dumbasPL <dumbasgaming@gmail.com>
2017-06-04 20:36:19 +0200

    Example date and time class for 4x20 display

--- Example.java
+++ Example.java
@@ -1,10 +1,11 @@
-package me.dumbasPL.lcd;
+//example for 4x20 display
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Main {
+public class Example {
 	
+	//custom chars
 	static byte[][] data = {{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x02 }, //lg
 	        				{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x00 }, //g
 	        				{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x08 }, //pg
@@ -17,11 +18,12 @@ public class Main {
 
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("LCD TEST!");
+		System.out.println("LCD DEMO!");
         
-		Lcd lcd = new Lcd((byte) 0x3F);
-		lcd.init();
-		lcd.loadCustom(data);
+		Lcd lcd = new Lcd((byte) 0x3F);//change 0x3f to your i2c adress
+		lcd.init(); //initialize display
+		lcd.loadCustom(data); //load custom chars
+		//display custom chars
 		lcd.custom(0, 0, 0);
 		for(int i = 1; i < 19; i++) lcd.custom(1, 0, i);
 		lcd.custom(2, 0, 19);
@@ -32,15 +34,12 @@ public class Main {
 		lcd.custom(5, 3, 0);
 		for(int i = 1; i < 19; i++) lcd.custom(7, 3, i);
 		lcd.custom(6, 3, 19);
-		
+		//start clock
 		while(true){
-			String date = new SimpleDateFormat("dd.MM.yyy").format(new Date());
-			String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
-			
-			lcd.string(date, 1, 5);
-			lcd.string(time, 2, 6);
-			
-			
+			String date = new SimpleDateFormat("dd.MM.yyy").format(new Date());//get date
+			String time = new SimpleDateFormat("HH:mm:ss").format(new Date());//get time
+			lcd.string(date, 1, 5);//display date in line 2 column 6
+			lcd.string(time, 2, 6);//display time in line 3 column 7
 		}
 		
 	}
